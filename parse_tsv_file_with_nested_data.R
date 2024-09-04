ca4ef0331c2fd2cc84ff549a11ad6b7270b26817
czhd-create <83773854+czhd-create@users.noreply.github.com>
2024-09-05 01:00:01 +0800

    Update parse_tsv_file_with_nested_data.R

--- parse_tsv_file_with_nested_data.R
+++ parse_tsv_file_with_nested_data.R
@@ -4,6 +4,8 @@ df <- readr::read_tsv(file.choose())
 
 text_list <- list()
 
+# assume the nested text file is in column 1 of the dataframe.
+# assmue that the different lines of the text is delimited by "\n\n".
 for (i in 1:nrow(df)) {
   ti <- unlist(df[i,1])
   split_texti <- strsplit(ti, "\n\n")
